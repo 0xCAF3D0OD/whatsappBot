4ee65bbfc28ee8cec0742807d373ce6d8d4b87a1
dino <kevin.dinocera@protonmail.com>
2024-09-11 13:18:19 +0200

    finalisation of the branche for the merge

--- 4_devPuppeteer/srcs/whatsapp-bot/routes/bot.js
+++ 4_devPuppeteer/srcs/whatsapp-bot/routes/bot.js
@@ -33,61 +33,6 @@ bot.get('/about', async (req, res) => {
     res.end();
 });
 
-// Exemple d'utilisation de Puppeteer
-// bot.get('/about/scrape', async (req, res) => {
-//     try {
-//         console.log(page);
-//         const content = await page.content();
-//         res.write(content);
-//         console.log("Page loaded successfully");
-//         try {
-//             await page.waitForSelector('div._akau', { timeout: 6000 }); // Attendre jusqu'à 60 secondes
-//
-//             // Extraire le QR code et le logo
-//             const { qrCodeData, logoSvg } = await page.evaluate(() => {
-//                 const canvas = document.querySelector('div._akau canvas');
-//                 const svg = document.querySelector('div._akau svg');
-//                 return {
-//                     qrCodeData: canvas.toDataURL(),
-//                     logoSvg: svg.outerHTML
-//                 };
-//             });
-//
-//             // Sauvegarder l'image du QR code
-//             const base64Data = qrCodeData.replace(/^data:image\/png;base64,/, "");
-//             await fs.writeFile("qr_code_temp.png", base64Data, 'base64');
-//
-//             // Créer une image du logo
-//             await fs.writeFile("logo_temp.svg", logoSvg);
-//
-//             // Utiliser sharp pour combiner le QR code et le logo
-//             await sharp("qr_code_temp.png")
-//                 .composite([{
-//                     input: "logo_temp.svg",
-//                     top: 100,
-//                     left: 100,
-//                     blend: 'over'
-//                 }])
-//                 .toFile("qr_code_with_logo.png");
-//
-//             console.log('QR Code avec logo sauvegardé dans qr_code_with_logo.png');
-//
-//             // Nettoyer les fichiers temporaires
-//             await fs.unlink("qr_code_temp.png");
-//             await fs.unlink("logo_temp.svg");
-//         }  catch (error) {
-//             console.error('Erreur lors de l\'attente du QR code:', error);
-//         }
-//     } catch (error) {
-//         console.error('Scraping error:', error);
-//         res.status(500).send(`Error occurred while scraping: ${error.message}`);
-//     } finally {
-//         // if (browser) {
-//         //     await browser.close();
-//         // }
-//     }
-// });
-
 bot.get(('/about/scrape'), async (req, res) => {
     try {
         console.log("chargement de la page...");
@@ -110,8 +55,6 @@ bot.get(('/about/scrape'), async (req, res) => {
 
         await sharp(qrCodeDataBuffer).composite([{
             input: logoSVGBuffer,
-            // top: 100,
-            // left: 100,
             gravity: 'center',
             blend: 'over'
         }]).toFile('qrCode.png');
@@ -119,33 +62,11 @@ bot.get(('/about/scrape'), async (req, res) => {
         console.log('QR Code avec logo sauvegardé comme qrCode.png');
         const qrCodePath = '/qrCode.png';
         res.sendFile(path.join(__dirname, '..', 'qrcode-template.html'));
-        // res.send("page chargee avec succes, " + await page.evaluate(() => {
-        //     const element = document.querySelector('svg[xmlns="http://www.w3.org/2000/svg"][width="64"][height="64"]');
-        //     if (element) {
-        //         return "la balise a bien ete chargee" + element;
-        //     }
-        //     else {
-        //         return "la balise n'a pas pu etre chargee";
-        //     }
-        // }));
     } catch (error) {
         console.error("Error during loading page");
         res.status(500).send(`Error during loading page ${error}`);
     }
 })
-// bot.get('/scrape/what', async (req, res) => {
-//     try {
-//         console.log(page);
-//         const content = await page.content();
-//         res.write(content);
-//         console.log("Page loaded successfully");    
-//         res.end();
-//     } catch (e) {
-//         console.error("Page error loading");
-//         res.status(500).send(`Error occurred while loading: ${e.message}`)
-//     }
-// });
-
 
 module.exports = bot;
 
